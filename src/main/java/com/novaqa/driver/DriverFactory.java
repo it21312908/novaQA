8e8a94cc5202d2f21b33f4f8d648ef8a30b36351
it21312908 <it21312908@my.sliit.lk>
2026-06-28 12:16:32 +0530

    Implement core framework with working Selenium execution using TestNG

---
+++ src/main/java/com/novaqa/driver/DriverFactory.java
@@ -0,0 +1,18 @@
+package com.novaqa.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
